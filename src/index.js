d8d625f32e7bd5924728eee7c4c78b1a32677b7c
Aline Favretto <aline.favretto@sap.com>
2019-09-04 23:59:59 -0300

    creates temis app

---
+++ src/index.js
@@ -0,0 +1,39 @@
+const bodyParser = require('body-parser');
+const watson = require('../src/watson/client-watson');
+const sessionId = "";
+const assistantId = '4e65423d-2127-4a94-b6b0-27163c60896c';
+
+const params = {
+  filter: 'language::en,request.context.metadata.deployment::testDeployment'
+};
+
+watson.listAllLogs(params)
+  .then(res => {
+    console.log(JSON.stringify(res, null, 2));
+  })
+  .catch(err => {
+    console.log(err)
+  });
+
+    // watson.createSession({
+    //     assistant_id: assistantId
+    // })
+    // .then(res => {
+    //   console.log(JSON.stringify(res, null, 2));
+    //   sessionId = res;
+    // })
+    // .catch(err => {
+    //   console.log(err);
+    // });
+
+
+    // watson.deleteSession({
+    //   assistant_id: assistantId,
+    //   session_id: sessionId,
+    // })
+    //   .then(res => {
+    //     console.log(JSON.stringify(res, null, 2));
+    //   })
+    //   .catch(err => {
+    //     console.log(err);
+    //   });
