b3dc659d0f414692888f1f8bf25fcd96255f3dec
Aline Favretto <aline.favretto@sap.com>
2019-09-05 00:48:21 -0300

    change to correct version

--- src/watson/client-watson.js
+++ src/watson/client-watson.js
@@ -2,7 +2,7 @@ const AssistantV1 = require('watson-developer-cloud/assistant/v1');
 const watsonAssistant = new AssistantV1({
   "iam_apikey": "mOS2ZvGUPguUYrui_8jvu5J-guRmOoliqiAgDHqvhtG2",
   "url": "https://gateway.watsonplatform.net/assistant/api",
-  "version":"Development"
+  "version":"2019-02-28"
 });
 
 module.exports = watsonAssistant;
