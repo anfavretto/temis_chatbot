d60b151c402ca5d55ab15a7f7ef887495d6139b8
Aline Favretto <aline.favretto@sap.com>
2019-09-05 00:37:45 -0300

    change assistant version

--- src/watson/client-watson.js
+++ src/watson/client-watson.js
@@ -1,5 +1,5 @@
-const AssistantV2 = require('watson-developer-cloud/assistant/v2');
-const watsonAssistant = new AssistantV2({
+const AssistantV1 = require('watson-developer-cloud/assistant/v1');
+const watsonAssistant = new AssistantV1({
   "iam_apikey": "mOS2ZvGUPguUYrui_8jvu5J-guRmOoliqiAgDHqvhtG2",
   "url": "https://gateway.watsonplatform.net/assistant/api",
   "version":"Development"
